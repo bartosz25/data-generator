29eeb7f478552a051b2497e7f2af09485e1ec9bf
bartosz25 <bartkonieczny@yahoo.fr>
2019-07-27 12:39:59 +0200

    make tests more variable

--- data_generator/test/sink/test_local_filesystem_writer.py
+++ data_generator/test/sink/test_local_filesystem_writer.py
@@ -16,9 +16,9 @@ def should_add_log_and_not_output_the_file():
         inactivity_delay=datetime.timedelta(minutes=10)
     )
 
-    configuration.add_log('test')
+    configuration.add_log('best')
 
-    files = [f for f in glob.glob(base_dir + "**/*.json", recursive=True)]
+    files = [f for f in glob.glob(base_dir + "/b/*.json", recursive=True)]
 
     assert_that(files).is_empty()
 
@@ -31,17 +31,17 @@ def should_add_log_and_write_output_file_after_timeout():
         base_dir=base_dir,
         inactivity_delay=datetime.timedelta(seconds=2)
     )
-    configuration.add_log('test')
+    configuration.add_log('xest')
     sleep(3)
-    configuration.add_log('aest2')
+    configuration.add_log('yest2')
     sleep(3)
-    configuration.add_log('xest2')
+    configuration.add_log('zest2')
 
-    files_for_t = [f for f in glob.glob(base_dir + "/t/*.json", recursive=True)]
-    files_for_a = [f for f in glob.glob(base_dir + "/a/*.json", recursive=True)]
+    files_for_x = [f for f in glob.glob(base_dir + "/x/*.json", recursive=True)]
+    files_for_y = [f for f in glob.glob(base_dir + "/y/*.json", recursive=True)]
 
-    assert_that(files_for_t).is_length(1)
-    assert_that(files_for_a).is_length(1)
+    assert_that(files_for_x).is_length(1)
+    assert_that(files_for_y).is_length(1)
 
 
 def should_add_2_logs_and_output_file_because_of_the_size_condition():
