e7f7f4ac541865c00d4494170384347ffc2c0f4c
bartosz25 <bartkonieczny@yahoo.fr>
2019-10-13 17:56:43 +0200

    load module to make it executable from the cli

--- examples/local_filesystem/write_sessions_to_local_filesystem.py
+++ examples/local_filesystem/write_sessions_to_local_filesystem.py
@@ -1,8 +1,12 @@
 import datetime
 import json
 import logging
+import os
 import random
 
+import sys
+
+sys.path.append(os.path.abspath(os.path.join('..', 'data-generator')))
 from data_generator.model.dataset import Dataset
 from data_generator.model.timer import Timer
 from data_generator.sink.local_filesystem_writer import LocalFileSystemConfiguration
@@ -10,8 +14,8 @@ from data_generator.sink.local_filesystem_writer import LocalFileSystemConfigura
 logging.basicConfig(filename='/tmp/logs_writer.txt', level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 if __name__ == '__main__':
-    timer = Timer(-3*24*60*60) # starting from 3 days ago
-    dataset = Dataset(duration_min_seconds=10, duration_max_minutes=10,
+    timer = Timer(-3*24*60*60)  # starting from 3 days ago
+    dataset = Dataset(duration_min_seconds=120, duration_max_minutes=10,
                       percentage_incomplete_data=2, percentage_inconsistent_data=2,
                       percentage_app_v1=20, percentage_app_v2=20,
                       users_number=10000, timer=timer
@@ -24,12 +28,13 @@ if __name__ == '__main__':
         json_object = json.loads(json_data)
         # TODO: try to format it with a datetime formatter
         event_time = json_object['event_time']
-        return f'{event_time[0:4]}/{event_time[5:7]}/{event_time[8:10]}/{event_time[11:13]}'
+        return '{day}/{month}/{year}/{hour}'.format(day=event_time[0:4], month=event_time[5:7],
+                                                    year=event_time[8:10], hour=event_time[11:13])
 
     configuration = LocalFileSystemConfiguration(
         partition_getter=_extract_event_time,
         max_in_partition=200000,
-        base_dir='/tmp/test_generator',
+        base_dir='/tmp/sessions/input',
         inactivity_delay=datetime.timedelta(minutes=10)
     )
 
