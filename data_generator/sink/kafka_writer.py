146eedeb202a50d2e45869260148ba93bd61089f
bartosz25 <bartkonieczny@yahoo.fr>
2020-05-02 08:54:56 +0200

    try to produce data with Kafka-Python library
    Confluent-Kafka is hard to install on Windows 10 because of some C issues

--- data_generator/sink/kafka_writer.py
+++ data_generator/sink/kafka_writer.py
@@ -1,9 +1,7 @@
-import time
-
+import json
 import logging
-from confluent_kafka.admin import AdminClient
-from confluent_kafka.cimpl import Producer, NewTopic
 
+from kafka import KafkaProducer
 
 logger = logging.getLogger(__name__)
 
@@ -22,51 +20,16 @@ class KafkaWriterConfiguration:
     def create_or_recreate_topics(self, admin=None):
         topics_to_recreate = list(filter(lambda topic_spec: topic_spec.recreate, self.topics))
         if topics_to_recreate:
-            logger.info('Got %s topics to recreate', topics_to_recreate)
-            if not admin:
-                admin = AdminClient({'bootstrap.servers': self.broker})
-            topics_to_delete = list(map(lambda topic_spec: topic_spec.name, topics_to_recreate))
-
-            deleted_topics_result = admin.delete_topics(topics_to_delete, operation_timeout=15, request_timeout=45)
-            for deleted_topic, future in deleted_topics_result.items():
-                try:
-                    future.result()
-                except Exception as e:
-                    # TODO: transform to WARN
-                    print('An error occurred on deleting {}: {}'.format(deleted_topic, e))
-
-            # Apparently there is a concurrent issue with topics create or recreate operations
-            # https://github.com/confluentinc/confluent-kafka-python/issues/524
-            # That's why I will try to create the topic 5 times with a kind of exponential backoff
-            topics_to_create = {topic_spec.name: topic_spec.to_new_topic() for topic_spec in topics_to_recreate}
-            creation_errors = []
-            for retry in range(0, 5):
-                if topics_to_create:
-                    futures = admin.create_topics(list(topics_to_create.values()),
-                                                  operation_timeout=15, request_timeout=45)
-                    for topic, f in futures.items():
-                        try:
-                            f.result()
-                            print("Topic {} created".format(topic))
-                            del topics_to_create[topic]
-                        except Exception as e:
-                            logger.warning("Topic %s was not created - retrying. Got exception %s",
-                                           topic, e, exc_info=1)
-                            if retry == 4:
-                                creation_errors.append((topic, e))
-                    time.sleep(retry*5)
-
-            assert not creation_errors, "Some topics were not correctly created {}".format(
-                list(map(lambda topic_and_error: "{}: {}".format(
-                    topic_and_error[0], topic_and_error[1]), creation_errors))
-            )
+            raise Exception("Topic recreation is not implemented in this version")
 
     def send_message(self, topic_name, key, message):
         if not self.producer:
             # check https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md for more information
-            producer_config = {'bootstrap.servers': self.broker}
-            producer_config.update(self.producer_configuration)
-            self.producer = Producer(producer_config)
+            producer_config = {'bootstrap_servers': self.broker,
+                               'value_serializer': lambda m: json.dumps(m).encode('utf-8')
+                               }
+            # TODO: imlement me later producer_config.update(self.producer_configuration)
+            self.producer = KafkaProducer(**producer_config)
         """
         Traceback (most recent call last):
         File "/home/bartosz/workspace/data-generator/examples/kafka/generate_dataset_to_kafka.py", line 51, in <module>
@@ -77,17 +40,17 @@ class KafkaWriterConfiguration:
         The below code is the workaround for the above problem, found here:
         `Confluent Kafka-Python issue 104 <https://github.com/confluentinc/confluent-kafka-python/issues/104>`
         """
-        def delivery_callback(error, result):
-            if error:
-                logger.error("Record was not correctly delivered: %s", error)
+
+        def delivery_callback(error):
+            logger.error("Record was not correctly delivered: %s", error)
+
         try:
-            self.producer.produce(topic=topic_name, key=bytes(key, encoding='utf-8'),
-                                  value=bytes(message, encoding='utf-8'),
-                                  on_delivery=delivery_callback)
+            self.producer.send(topic=topic_name, key=key.encode('utf-8'),
+                               value=json.loads(message)).add_errback(delivery_callback)
         except BufferError:
             self.producer.flush()
-            self.producer.produce(topic=topic_name, key=bytes(key, encoding='utf-8'),
-                                  value=bytes(message, encoding='utf-8'), on_delivery=delivery_callback)
+            self.producer.send(topic=topic_name, key=key.encode('utf-8'),
+                               value=json.loads(message)).add_errback(delivery_callback)
 
     def __repr__(self):
         return 'KafkaWriterConfiguration (broker={}) (topics={})'.format(self.broker, self.topics)
@@ -101,7 +64,9 @@ class KafkaConfigurationTopic:
         self.recreate = recreate
 
     def to_new_topic(self):
-        return NewTopic(self.name, num_partitions=self.partitions, replication_factor=self.replication)
+        # TODO: put back later return NewTopic
+        # (self.name, num_partitions=self.partitions, replication_factor=self.replication)
+        pass
 
     def __repr__(self):
         return 'Kafka topic {} (repl={}, part={}, recreate={})'.format(self.name, self.replication,
